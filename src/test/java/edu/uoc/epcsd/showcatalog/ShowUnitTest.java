8894c84d59eea1f7e2d77f71df35a1015e80a945
Jerónimo López Ledesma <jeronimo.lopez@dekra.com>
2022-05-18 07:39:29 +0200

    Initial commit

---
+++ src/test/java/edu/uoc/epcsd/showcatalog/ShowUnitTest.java
@@ -0,0 +1,18 @@
+package edu.uoc.epcsd.showcatalog;
+
+import edu.uoc.epcsd.showcatalog.domain.Show;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShowUnitTest {
+
+    private Show show;
+
+
+
+
+
+
+}
